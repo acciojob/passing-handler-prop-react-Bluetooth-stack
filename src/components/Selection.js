dca9ecf6988f28c14db9c3db1b2e24e906f98537
Siddharth Hota <siddharthhota26@gmail.com>
2023-08-16 13:42:37 +0530

    initial

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React, { useState } from 'react'
+import '../styles/Child.css'
 
+function Selection(props) {
+    const [color, setColor] = useState('');
+  return (
+    <div className='fix-box' onClick={()=>{props.applyColor(setColor)}} style={{background: color}}>
+        <h2 className='subheading'>Selection</h2>
+    </div>
+  )
+}
+
+export default Selection
