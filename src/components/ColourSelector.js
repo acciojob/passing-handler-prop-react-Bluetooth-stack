7a8a9989b73a7deba5036b44f7ab16e8639cacca
Siddharth Hota <siddharthhota26@gmail.com>
2023-08-16 14:01:32 +0530

    again

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -3,8 +3,10 @@ import React from 'react';
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
   const { background } = config;
+  const { key } = config;
   return (
-    <button className={props.classname} onClick={() => selectNextBackground({background: background})}>
+    <button className={props.classname} onClick={() => selectNextBackground({background: background})}
+    data-testid={key}>
       {props.label}
     </button>
   )
