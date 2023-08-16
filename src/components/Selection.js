cff314c7cb12bc711b2a9b0f70df08f0f5df5c4e
Siddharth Hota <siddharthhota26@gmail.com>
2023-08-16 13:50:48 +0530

    data attribute

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import '../styles/Child.css'
 
 function Selection(props) {
-    const [color, setColor] = useState('');
+    const [color, setColor] = useState({});
   return (
-    <div className='fix-box' onClick={()=>{props.applyColor(setColor)}} style={{background: color}}>
+    <div className='fix-box' onClick={()=>{props.applyColor(setColor)}} style={{background: color.color}} data-testid={color.attribute}>
         <h2 className='subheading'>Selection</h2>
     </div>
   )
